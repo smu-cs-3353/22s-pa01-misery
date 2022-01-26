dd13c5ab2d869d87af8a61f1ed3f5085b8f732b6
Mikecamdo <mcdoherty@smu.edu>
2022-01-26 16:59:45 -0600

    Set up main.cpp and CMakeLists
    Set up main.cpp and CMakeLists

---
+++ cmake-build-debug/CMakeFiles/22s_pa01_misery.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "22s_pa01_misery"
+  "22s_pa01_misery.pdb"
+  "CMakeFiles/22s_pa01_misery.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/22s_pa01_misery.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by mikec_g1kgiu8 on 1/26/2022.
+//
+
+using namespace std;
+
+#include <iostream>
+
+int main() {
+    cout << "Hello World" << endl;
+}
+
