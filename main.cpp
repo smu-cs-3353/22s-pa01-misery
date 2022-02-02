a01341821f709c5d276489a6f8418252ac24b0b2
Mikecamdo <mcdoherty@smu.edu>
2022-02-02 13:24:44 -0600

    File Read In and Early Version of Brute Force
    Changed the data to being read from a file. Also implemented an early version of the brute force algorithm.

--- main.cpp
+++ main.cpp
@@ -8,18 +8,11 @@
 
 using namespace std;
 
-vector<vector<int>> getSubsets(vector<int>);
+vector<vector<int>> findOptimalValue(vector<int>, vector<int>, vector<int>, vector<int>);
 
 
 int main(int argc, char** argv) {
     //Brute Force Algorithm
-//    int wallLength = 1024; //FIXME need to change to reading in from a file
-//    int wallHeight = 768;
-//    int numArtPieces = 4;
-//    vector<int> pictureIDs = {12, 9, 23, 1};
-//    vector<int> pictureValues = {300, 200, 250, 100};
-//    vector<int> pictureWidths = {1023, 300, 800, 100};
-//    vector<int> pictureHeights = {767, 100, 700, 100};
     ifstream input(argv[1]);
 
     int wallLength, wallHeight, numArtPieces;
@@ -37,30 +30,80 @@ int main(int argc, char** argv) {
         pictureHeights.push_back(pictureHeight);
     }
 
-    vector<vector<int>> subsets = getSubsets(pictureIDs);
-    for (int i = 0; i < subsets.size(); i++) {
-        for (int j = 0; j < subsets[i].size(); j++) {
-            cout << subsets[i][j] << " ";
-        }
-        cout << endl;
+    vector<vector<int>> theOptimalSolution = findOptimalValue(pictureIDs, pictureValues, pictureWidths, pictureHeights);
+
+    cout << theOptimalSolution.at(0).at(0) << endl;
+    vector<int> optimalIDs = theOptimalSolution.at(1);
+    vector<int> optimalValues = theOptimalSolution.at(2);
+    vector<int> optimalWidths = theOptimalSolution.at(3);
+    vector<int> optimalHeights = theOptimalSolution.at(4);
+
+    for (int i = 0; i < optimalIDs.size(); i++) {
+        cout << optimalIDs.at(i) << " " << optimalValues.at(i) << " " << optimalWidths.at(i) << " " << optimalHeights.at(i) << endl;
     }
+
 }
 
-vector<vector<int>> getSubsets(vector<int> pictureIDs) { //inspired by code found at: https://stackoverflow.com/questions/728972/finding-all-the-subsets-of-a-set
-    vector<vector<int>> subsets;
+vector<vector<int>> findOptimalValue(vector<int> pictureIDs, vector<int> pictureValues, vector<int> pictureWidths, vector<int> pictureHeights) { //inspired by code found at: https://stackoverflow.com/questions/728972/finding-all-the-subsets-of-a-set
+    vector<vector<int>> IDSubsets;
+    vector<vector<int>> valueSubsets;
+    vector<vector<int>> widthSubsets;
+    vector<vector<int>> heightSubsets;
+
     vector<int> empty;
-    subsets.push_back(empty);
+    IDSubsets.push_back(empty);
+    valueSubsets.push_back(empty);
+    widthSubsets.push_back(empty);
+    heightSubsets.push_back(empty);
+
+    int bestPrice = -1;
+    vector<vector<int>> results;
+
 
     for (int i = 0; i < pictureIDs.size(); i++) {
-        vector<vector<int>> temp = subsets;
+        vector<vector<int>> IDTemp = IDSubsets;
+        vector<vector<int>> valueTemp = valueSubsets;
+        vector<vector<int>> widthTemp = widthSubsets;
+        vector<vector<int>> heightTemp = heightSubsets;
 
-        for (int j = 0; j < temp.size(); j++) {
-            temp[j].push_back(pictureIDs[i]);
+        int currentPrice;
+//        cout << "Size: " << temp.size() << endl;
+
+        for (int j = 0; j < IDTemp.size(); j++) {
+            IDTemp[j].push_back(pictureIDs[i]);
+            valueTemp[j].push_back(pictureValues[i]);
+            widthTemp[j].push_back(pictureWidths[i]);
+            heightTemp[j].push_back(pictureHeights[i]);
         }
-        for (int j = 0; j < temp.size(); j++) {
-            subsets.push_back(temp[j]);
+        for (int j = 0; j < IDTemp.size(); j++) {
+            currentPrice = 0;
+            for (int k = 0; k < IDTemp[j].size(); k++) {
+//                cout << IDTemp[j][k] << " ";
+                currentPrice += valueTemp[j][k];
+            }
+//            cout << endl;
+
+            if (currentPrice > bestPrice) {
+                vector<vector<int>> theBest;
+                vector<int> theBestPrice;
+                theBestPrice.push_back(currentPrice);
+                bestPrice = currentPrice;
+
+                theBest.push_back(theBestPrice);
+                theBest.push_back(IDTemp[j]);
+                theBest.push_back(valueTemp[j]);
+                theBest.push_back(widthTemp[j]);
+                theBest.push_back(heightTemp[j]);
+
+                results = theBest;
+            }
+
+            IDSubsets.push_back(IDTemp[j]);
+            valueSubsets.push_back(valueTemp[j]);
+            widthSubsets.push_back(widthTemp[j]);
+            heightSubsets.push_back(heightTemp[j]);
         }
     }
 
-    return subsets;
+    return results;
 }
