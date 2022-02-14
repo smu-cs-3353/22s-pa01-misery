e79b6d94126e1cefc7dcfcdd1d646a94119cd1ec
Mikecamdo <mcdoherty@smu.edu>
2022-02-14 01:13:33 -0600

    Fixed main.cpp and Input/Output Files
    Fixed an issue in main.cpp where there were two initializations of the brute force algorithm (caused by an oversight when merging branches). Also corrected the input files and output files

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,7 @@
 //
 // Created by mikec_g1kgiu8 on 1/26/2022.
 //
-#include <cmath> //FIXME is this used?
-#include <vector>
 #include <iostream>
-#include <fstream>
-#include <chrono>
 #include "BruteForceAlgo.h"
 #include "MostExpensiveFirstAlgo.h"
 #include "CustomAlgo.h"
@@ -18,38 +14,14 @@ int main(int argc, char** argv) {
         cout << "Error: Command line argument needed. Enter your command line argument after the executable." << endl;
         cout << "Example (output is the executable name): ./output ../input/PictureInfo.txt" << endl;
     } else {
-
-        std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
-
         BruteForceAlgo firstAlgorithm;
         firstAlgorithm.findOptimalPlacement(argv);
 
-
-        start = std::chrono::high_resolution_clock::now();
-        BruteForceAlgo firstAlgorithm;
-        firstAlgorithm.findOptimalPlacement(argv);
-        end = std::chrono::high_resolution_clock::now();
-
-        std::chrono::duration<double> time_in_seconds = end - start;
-        cout << fixed << "BruteForceAlgo Duration: " << time_in_seconds.count() << endl;
-
-
-        start = std::chrono::high_resolution_clock::now();
         MostExpensiveFirstAlgo secondAlgorithm;
         secondAlgorithm.findOptimalPlacement(argv);
-        end = std::chrono::high_resolution_clock::now();
 
-
-        /*std::chrono::duration<double>*/ time_in_seconds = end - start;
-        cout << fixed << "MostExpensiveFirstAlgo Duration: " << time_in_seconds.count() << endl;
-
-        start = std::chrono::high_resolution_clock::now();
         CustomAlgo thirdAlgorithm;
         thirdAlgorithm.findOptimalPlacement(argv);
-        end = std::chrono::high_resolution_clock::now();
-
-        time_in_seconds = end - start;
-        cout << fixed << "CustomAlgo Duration: " << time_in_seconds.count() << endl;
     }
 
     return 0;
