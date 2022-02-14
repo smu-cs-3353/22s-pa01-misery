ed073430849294e10248f45f78810eb188e11b91
Mikecamdo <mcdoherty@smu.edu>
2022-02-13 22:05:02 -0600

    Added comments
    Added comments to brute force algorithm

--- src/BruteForceAlgo.cpp
+++ src/BruteForceAlgo.cpp
@@ -7,13 +7,18 @@
 void BruteForceAlgo::findOptimalPlacement(char** argv) {
     ifstream input(argv[1]);
 
+    if (!input.is_open()) {
+        cout << "Could not open " << argv[1] << endl;
+        return;
+    }
+
     int wallLength, wallHeight, numArtPieces;
-    input >> wallLength >> wallHeight >> numArtPieces;
+    input >> wallLength >> wallHeight >> numArtPieces; //wall data + number of art pieces
 
     vector<int> pictureIDs, pictureValues, pictureWidths, pictureHeights;
     int pictureID, pictureValue, pictureWidth, pictureHeight;
 
-    for (int i = 0; i < numArtPieces; i++) {
+    for (int i = 0; i < numArtPieces; i++) { //reads all the picture data and stores it in vectors
         input >> pictureID >> pictureValue >> pictureWidth >> pictureHeight;
 
         pictureIDs.push_back(pictureID);
@@ -22,7 +27,9 @@ void BruteForceAlgo::findOptimalPlacement(char** argv) {
         pictureHeights.push_back(pictureHeight);
     }
 
-    vector<vector<int>> theOptimalSolution = findOptimalValue(pictureIDs, pictureValues, pictureWidths, pictureHeights, wallLength);
+    //finds the best possible solution through brute force
+    vector<vector<int>> theOptimalSolution = findOptimalValue(pictureIDs, pictureValues,
+                                                              pictureWidths, pictureHeights, wallLength);
 
     string fileName = argv[1];
     for (int i = 0; i < 4; i++) { //gets rid of the .txt in fileName
@@ -31,35 +38,36 @@ void BruteForceAlgo::findOptimalPlacement(char** argv) {
     fileName += "-bruteforce.txt";
     ofstream output(fileName);
 
-    output << theOptimalSolution.at(0).at(0) << endl;
+    output << theOptimalSolution.at(0).at(0) << endl; //outputs the total value of the pictures on the wall
     vector<int> optimalIDs = theOptimalSolution.at(1);
     vector<int> optimalValues = theOptimalSolution.at(2);
     vector<int> optimalWidths = theOptimalSolution.at(3);
     vector<int> optimalHeights = theOptimalSolution.at(4);
 
 
-    for (int i = 0; i < optimalIDs.size(); i++) {
+    for (int i = 0; i < optimalIDs.size(); i++) { //outputs the data of the pictures on the wall
         output << optimalIDs.at(i) << " " << optimalValues.at(i) << " " << optimalWidths.at(i) << " " << optimalHeights.at(i) << endl;
     }
 
     output.close();
 }
 
-vector<vector<int>> BruteForceAlgo::findOptimalValue(vector<int> pictureIDs, vector<int> pictureValues, vector<int> pictureWidths, vector<int> pictureHeights, int wallLength) { //inspired by code found at: https://stackoverflow.com/questions/728972/finding-all-the-subsets-of-a-set
-    vector<vector<int>> IDSubsets;
-    vector<vector<int>> valueSubsets;
-    vector<vector<int>> widthSubsets;
-    vector<vector<int>> heightSubsets;
+//inspired by code found at: https://stackoverflow.com/questions/728972/finding-all-the-subsets-of-a-set
+vector<vector<int>> BruteForceAlgo::findOptimalValue(vector<int> pictureIDs, vector<int> pictureValues, vector<int> pictureWidths,
+                                                     vector<int> pictureHeights, int wallLength) {
+    vector<vector<int>> IDSubsets; //all subsets for picture IDs
+    vector<vector<int>> valueSubsets; //all subsets for picture values
+    vector<vector<int>> widthSubsets; //all subsets for picture widths
+    vector<vector<int>> heightSubsets; //all subsets for picture heights
 
-    vector<int> empty;
+    vector<int> empty; //the empty subset
     IDSubsets.push_back(empty);
     valueSubsets.push_back(empty);
     widthSubsets.push_back(empty);
     heightSubsets.push_back(empty);
 
-    int bestPrice = -1;
+    int bestPrice = -1; //initializing bestPrice
     vector<vector<int>> results;
-
 
     for (int i = 0; i < pictureIDs.size(); i++) {
         vector<vector<int>> IDTemp = IDSubsets;
@@ -69,21 +77,21 @@ vector<vector<int>> BruteForceAlgo::findOptimalValue(vector<int> pictureIDs, vec
 
         int currentPrice, currentLength;
 
-        for (int j = 0; j < IDTemp.size(); j++) {
+        for (int j = 0; j < IDTemp.size(); j++) { //loop size doubles every time i increases
             IDTemp[j].push_back(pictureIDs[i]);
             valueTemp[j].push_back(pictureValues[i]);
             widthTemp[j].push_back(pictureWidths[i]);
             heightTemp[j].push_back(pictureHeights[i]);
         }
-        for (int j = 0; j < IDTemp.size(); j++) {
+        for (int j = 0; j < IDTemp.size(); j++) { //loop size doubles every time i increases
             currentPrice = 0;
             currentLength = 0;
             for (int k = 0; k < IDTemp[j].size(); k++) {
-                currentPrice += valueTemp[j][k];
-                currentLength += widthTemp[j][k];
+                currentPrice += valueTemp[j][k]; //calculates the current subset's total price
+                currentLength += widthTemp[j][k]; //calculates the current subset's total width
             }
 
-            if (currentPrice > bestPrice && currentLength <= wallLength) {
+            if (currentPrice > bestPrice && currentLength <= wallLength) { //if a better solution is found
                 vector<vector<int>> theBest;
                 vector<int> theBestPrice;
                 theBestPrice.push_back(currentPrice);
@@ -95,7 +103,7 @@ vector<vector<int>> BruteForceAlgo::findOptimalValue(vector<int> pictureIDs, vec
                 theBest.push_back(widthTemp[j]);
                 theBest.push_back(heightTemp[j]);
 
-                results = theBest;
+                results = theBest; //stores the better solution
             }
 
             IDSubsets.push_back(IDTemp[j]);
