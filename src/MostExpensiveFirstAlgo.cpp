2f0f080f45fb3b61acc3ed739d6986176b12388d
Mikecamdo <mcdoherty@smu.edu>
2022-02-13 22:18:43 -0600

    Added Comments
    Added comments to the most expensive first algorithm

--- src/MostExpensiveFirstAlgo.cpp
+++ src/MostExpensiveFirstAlgo.cpp
@@ -7,13 +7,19 @@
 void MostExpensiveFirstAlgo::findOptimalPlacement(char** argv) {
     ifstream input(argv[1]);
 
+    if (!input.is_open()) {
+        cout << "Could not open " << argv[1] << endl;
+        return;
+    }
+
+
     int wallLength, wallHeight, numArtPieces;
-    input >> wallLength >> wallHeight >> numArtPieces;
+    input >> wallLength >> wallHeight >> numArtPieces; //wall data + number of art pieces
 
     vector<int> pictureIDs, pictureValues, pictureWidths, pictureHeights;
     int pictureID, pictureValue, pictureWidth, pictureHeight;
 
-    for (int i = 0; i < numArtPieces; i++) {
+    for (int i = 0; i < numArtPieces; i++) { //reads all the picture data and stores it in vectors
         input >> pictureID >> pictureValue >> pictureWidth >> pictureHeight;
 
         pictureIDs.push_back(pictureID);
@@ -22,8 +28,10 @@ void MostExpensiveFirstAlgo::findOptimalPlacement(char** argv) {
         pictureHeights.push_back(pictureHeight);
     }
 
+    //sorts the picture data vectors from highest value to lowest value
     sortByMostExpensive(pictureIDs, pictureValues, pictureWidths, pictureHeights, 0, pictureValues.size() - 1);
 
+    //finds the solution
     vector<vector<int>> theOptimalSolution = findOptimalPlacement(pictureIDs, pictureValues, pictureWidths, pictureHeights, wallLength);
 
 
@@ -35,9 +43,9 @@ void MostExpensiveFirstAlgo::findOptimalPlacement(char** argv) {
 
     ofstream output(fileName);
 
-    output << theOptimalSolution.at(theOptimalSolution.size() - 1).at(0) << endl;
+    output << theOptimalSolution.at(theOptimalSolution.size() - 1).at(0) << endl; //outputs the total value of the pictures on the wall
 
-    for (int i = 0; i < theOptimalSolution.size() - 1; i++) {
+    for (int i = 0; i < theOptimalSolution.size() - 1; i++) { //outputs the data of the pictures on the wall
         vector<int> temp = theOptimalSolution.at(i);
         for (int j = 0; j < temp.size(); j++) {
             output << temp.at(j) << " ";
@@ -50,22 +58,22 @@ void MostExpensiveFirstAlgo::findOptimalPlacement(char** argv) {
 
 vector<vector<int>> MostExpensiveFirstAlgo::findOptimalPlacement(vector<int> pictureIDs, vector<int> pictureValues, vector<int> pictureWidths, vector<int> pictureHeights, int wallLength) {
     vector<vector<int>> theSolution;
-    int totalPrice = 0;
+    int totalPrice = 0; //stores the total value of the pictures on the wall
     for (int i = 0; i < pictureValues.size(); i++) {
-        if (pictureWidths.at(i) <= wallLength) {
-            wallLength -= pictureWidths.at(i);
+        if (pictureWidths.at(i) <= wallLength) { //if the picture fits on the wall
+            wallLength -= pictureWidths.at(i); //remove the size of the picture from the remaining wall space
 
             vector<int> temp = {pictureIDs.at(i), pictureValues.at(i), pictureWidths.at(i), pictureHeights.at(i)};
-            theSolution.push_back(temp);
-            totalPrice += pictureValues.at(i);
-        } else if (wallLength <= 0) {
+            theSolution.push_back(temp); //add the picture to theSolution
+            totalPrice += pictureValues.at(i); //update the total price
+        } else if (wallLength <= 0) { //if the wall is full
             break;
         }
     }
-    vector<int> temp = {totalPrice};
+    vector<int> temp = {totalPrice}; //store the total price
     theSolution.push_back(temp);
 
-    return theSolution;
+    return theSolution; //return theSolution
 }
 
 //The following sorting algorithm is a quick sort algorithm inspired by code found at: https://www.geeksforgeeks.org/quick-sort/
@@ -99,6 +107,7 @@ int MostExpensiveFirstAlgo::partition(vector<int>& pictureIDs, vector<int>& pict
     return i + 1;
 }
 
+//swaps the values of a and b
 void MostExpensiveFirstAlgo::swap(int* a, int* b) {
     int temp = *a;
     *a = *b;
